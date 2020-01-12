d5b7bd10f4d80d36bb8256ed629d1352142897dd
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-12 18:08:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,5 +43,5 @@ end
 def self.create_from_filename(name)
   song = self.new
   song.name = (name.split(" - ")[1].chomp(".mp3"))
-  song.artist_name = (name.split)
+  song.artist_name = (name.split(" - ")[0])
 end
