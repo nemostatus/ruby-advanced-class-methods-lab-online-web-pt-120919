0f8e0ee7df750986c816112b6e82409406eec57e
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-12 17:45:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
     @@all.find{|x|x.name == name}
   end
   def self.find_or_create_by_name(name)
-    self.find_by_name(name)||self.create_by_name
+    self.find_by_name(name)||self.create_by_name(name)
   end
 
 end
